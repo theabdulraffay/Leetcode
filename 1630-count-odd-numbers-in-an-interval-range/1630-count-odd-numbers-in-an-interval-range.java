b9f84a2e7857735889553919bf16b2b1c54ecd28
theabdulraffay <81872649+theabdulraffay@users.noreply.github.com>
2023-10-06 23:03:32 +0500

    Time: 0 ms (100%), Space: 39.6 MB (9.91%) - LeetHub

--- 1630-count-odd-numbers-in-an-interval-range/1630-count-odd-numbers-in-an-interval-range.java
+++ 1630-count-odd-numbers-in-an-interval-range/1630-count-odd-numbers-in-an-interval-range.java
@@ -1,12 +1,5 @@
 class Solution {
     public int countOdds(int low, int high) {
-        int n = 0;
-        for(int i = low; i <= high; i++){
-            if(i%2 ==1){
-                i++;
-                n++;
-            }
-        }
-        return n;
+        return high%2==0 ? (high-low+1)/2 : (high-low+2)/2;
     }
 }
