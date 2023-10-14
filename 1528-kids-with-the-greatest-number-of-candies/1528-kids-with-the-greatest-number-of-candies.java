936ba25829babd3ec8193f7ccb993f6deed3d5f4
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2023-10-14 16:22:07 +0500

    Time: 1 ms (93.93%), Space: 41.3 MB (72.51%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int m = 0;
+        for(int i = 0; i < candies.length; i++){
+            candies[i] = candies[i] + extraCandies;
+            if(m < candies[i]){m = candies[i];}
+        }
+        m -= extraCandies;
+
+        ArrayList<Boolean> list = new ArrayList<Boolean>() ;
+        for(int i = 0; i < candies.length; i++){
+            if(candies[i] >= m){list.add(true);}else{list.add(false);}
+        }
+        return list;
+        
+    }
+}
