34244d5da5a62b756124b0a44a89fe270ad23196
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2023-10-16 22:48:21 +0500

    Time: 2 ms (68.54%), Space: 44.8 MB (87.44%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+     public void moveZeroes(int[] nums) {
+        int n = 0;
+		for(int i=0;i<nums.length;i++){
+			if(nums[i] == 0){
+				n++;
+				continue;
+			}
+			else if(n>0){
+				int t = nums[i];
+				nums[i] = 0;
+				nums[i-n] = t;
+
+			}
+
+		}
+    }
+}
