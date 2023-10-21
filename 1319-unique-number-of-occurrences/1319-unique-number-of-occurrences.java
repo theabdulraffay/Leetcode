b326d76522b65c2ac962b701c1fd087fb1102535
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2023-10-21 21:28:11 +0500

    Time: 8 ms (5.31%), Space: 40.1 MB (95.03%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> nlist = new ArrayList<Integer>();
+
+        for(int i : arr){
+            if(list.contains(i)){continue;}
+            else{list.add(i);}
+        }
+        //System.out.println(list);
+
+
+        for(int i = 0; i< list.size();i++){
+            int counter = 0;
+            for(int k = 0; k <arr.length;k++){
+                if(list.get(i) == arr[k]){counter++;}
+            }
+            nlist.add(counter);
+        }
+        //System.out.println(nlist);
+        for(int i = 0; i<nlist.size();i++){
+            for(int j = 0; j<nlist.size();j++){
+                if(i == j)continue;
+                if(nlist.get(i) == nlist.get(j)){
+                    return false;
+                    
+                }
+            }
+        }
+        //System.out.println(true);
+        return true;   
+    }
+}
