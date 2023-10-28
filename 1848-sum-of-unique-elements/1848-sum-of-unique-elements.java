0f8d12cf8556fa5e042bda3819aad8a416edf503
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2023-10-28 22:16:26 +0500

    Time: 2 ms (24.59%), Space: 40.3 MB (29.5%) - LeetHub

---
+++ 1848-sum-of-unique-elements/1848-sum-of-unique-elements.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int sum = 0;
+		for (int i = 0;i<nums.length ;i++ ) {
+			int counter =0;
+			for(int j =0;j<nums.length;j++){
+				if(i == j)continue;
+				if(nums[i] == nums[j])
+				{counter++;}
+
+
+
+			}
+			if(counter == 0){
+				sum += nums[i];
+			}
+			
+		}
+        return sum;
+        
+    }
+}
