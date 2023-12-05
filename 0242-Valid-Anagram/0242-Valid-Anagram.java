0f3e249c52c2d209415b81aae4100b8ef3d31048
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2023-12-05 14:48:16 +0500

    Time: 3 ms (38.30%), Space: 43.49 MB (49.74%) - LeetHub

--- 0242-Valid-Anagram/0242-Valid-Anagram.java
+++ 0242-Valid-Anagram/0242-Valid-Anagram.java
@@ -1,25 +1,37 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
-			return false;
-		}
-		char[] temp = new char[s.length()];
-		char[] temp2 = new char[s.length()];
-        for(int i =0;i<s.length();i++){
-            temp[i] = s.charAt(i);
-            temp2[i] = t.charAt(i);
+        if(s.length() != t.length())return false;
+        char[] c1 = s.toCharArray();
+        char[] c2 = t.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+
+        for(int i = 0; i < c1.length; i++) {
+        	if(c1[i] != c2[i]){
+        		return(false);
+        	}
         }
-        Arrays.sort(temp2);
-        Arrays.sort(temp);
-        // for (int i = 0;i<s.length();i++ ) {
-        	
-        // 	if(temp[i] != temp2[i]) {
-        // 		return false;
-        		
-        // 	}
-        	
+        return(true);
+        // if(s.length() != t.length()){
+		// 	return false;
+		// }
+		// char[] temp = new char[s.length()];
+		// char[] temp2 = new char[s.length()];
+        // for(int i =0;i<s.length();i++){
+        //     temp[i] = s.charAt(i);
+        //     temp2[i] = t.charAt(i);
         // }
-        return Arrays.equals(temp,temp2);
+        // Arrays.sort(temp2);
+        // Arrays.sort(temp);
+        // // for (int i = 0;i<s.length();i++ ) {
+        	
+        // // 	if(temp[i] != temp2[i]) {
+        // // 		return false;
+        		
+        // // 	}
+        	
+        // // }
+        // return Arrays.equals(temp,temp2);
 
         
     }
