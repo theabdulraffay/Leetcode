8085e06527d4f4d91b28275547437859f43d79b6
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2023-12-08 23:05:39 +0500

    Time: 1 ms (99.02%), Space: 44.26 MB (41.52%) - LeetHub

--- 0724-Find-Pivot-Index/0724-Find-Pivot-Index.java
+++ 0724-Find-Pivot-Index/0724-Find-Pivot-Index.java
@@ -7,16 +7,30 @@ class Solution {
         return count;
     }
     public int pivotIndex(int[] nums) {
-        int start = 0;
-        int end = nums.length;
-        for(int i = 0;i<end;i++){
-            int sum1 = sum(nums, 0,i);
-            int sum2 = sum(nums,i+1,nums.length);
-            if(sum1 == sum2){
+        int sum =0;
+        for(int i:nums) sum += i;
+
+        int sum2 = 0;
+        for(int i=0;i<nums.length;i++){
+            sum -= nums[i];
+
+            if(sum == sum2){
                 return i;
             }
+            sum2 += nums[i];
+
         }
         return -1;
+        // int start = 0;
+        // int end = nums.length;
+        // for(int i = 0;i<end;i++){
+        //     int sum1 = sum(nums, 0,i);
+        //     int sum2 = sum(nums,i+1,nums.length);
+        //     if(sum1 == sum2){
+        //         return i;
+        //     }
+        // }
+        // return -1;
         
     }
 }
