8375d127058ad65f418ce569977892d6b701f88a
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2023-12-26 00:17:42 +0500

    Time: 5 ms (29.22%), Space: 41.84 MB (18.51%) - LeetHub

--- 1758-Minimum-Changes-To-Make-Alternating-Binary-String/1758-Minimum-Changes-To-Make-Alternating-Binary-String.java
+++ 1758-Minimum-Changes-To-Make-Alternating-Binary-String/1758-Minimum-Changes-To-Make-Alternating-Binary-String.java
@@ -6,11 +6,11 @@ class Solution {
 		int c = 0, p = 0;
 		for(int i = 0;i<s.length();i++){
             if(i % 2 == 0){
-                if(s.charAt(i) != f){c++;}
-                if(s.charAt(i) != l){p++;}
+                if(s.charAt(i) != f)c++;
+                if(s.charAt(i) != l)p++;
             } else{
-                if(s.charAt(i) != l){c++;}
-                if(s.charAt(i) != f){p++;}
+                if(s.charAt(i) != l)c++;
+                if(s.charAt(i) != f)p++;
             }
 		}
         return Math.min(c,p);
