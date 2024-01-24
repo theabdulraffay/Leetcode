2724bc944546b7f3a2103a28d539abaef1de887a
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2024-01-25 03:49:15 +0500

    Time: 11 ms (45.28%), Space: 56.68 MB (46.04%) - LeetHub

---
+++ 0148-Sort-List/0148-Sort-List.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode ans = new ListNode();
+        ListNode tail = ans;
+		while(list1 != null && list2 != null){
+			if(list1.val < list2.val){
+				tail.next = list1;
+                tail = list1;
+				list1 = list1.next;
+			}else{
+				tail.next = list2;
+                tail = list2;
+				list2 = list2.next;
+			}
+		}
+
+		while(list1 != null){
+			tail.next = list1;
+                tail = list1;
+				list1 = list1.next;
+		}
+
+
+		while(list2 != null){
+			tail.next = list2;
+                tail = list2;
+				list2 = list2.next;
+		}
+		return ans.next;
+        
+    }
+
+    public ListNode middleNode(ListNode head){
+        ListNode slow = null;
+        while(head != null && head.next != null){
+            slow = (slow == null) ? head : slow.next;
+            head = head.next.next;
+        }
+        ListNode mid = slow.next;
+        slow.next = null;
+        return mid;
+    	// ListNode fast = head;
+    	// ListNode slow = head;
+    	// while(fast != null && fast.next != null){
+    	// 	fast = fast.next.next;
+    	// 	slow = slow.next;
+
+    	// }
+    	// return slow;
+    }
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null){
+    		return head;
+    	}
+    	ListNode mid = middleNode(head);
+    	ListNode left = sortList(head);
+    	ListNode right = sortList(mid);
+    	ListNode temp = mergeTwoLists(right,left);
+
+    	return temp;
+        
+    }
+}
