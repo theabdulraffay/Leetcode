c044919d282c1005f7f3cbaec84e60c11eeed2cf
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2024-02-04 08:27:37 +0500

    Time: 0 ms (100.00%), Space: 41.40 MB (14.28%) - LeetHub

--- 0033-Search-in-Rotated-Sorted-Array/0033-Search-in-Rotated-Sorted-Array.java
+++ 0033-Search-in-Rotated-Sorted-Array/0033-Search-in-Rotated-Sorted-Array.java
@@ -1,12 +1,55 @@
 class Solution {
+    static int pivotOfRotatedArray(int[] array){
+		int start = 0;
+		int end = array.length - 1;
+		while(start <= end){
+			int mid = start + (end - start) / 2;
+			if(mid < end && array[mid] > array[mid + 1]){
+				return mid; 
+			}else if(mid > start && array[mid] < array[mid - 1]){
+				return mid -1;
+			}
+			else if(array[start] > array[mid]){
+				end = mid -1;
+			}else{
+				start = mid + 1;
+			}
+		}
+		return -1;
+	}
+    static int index(int[] n, int target, int start, int end){
+		while(start <= end){
+			// int mid = (start + end)/2;
+			int mid = start + (end - start)/2;
+			if(n[mid] == target){
+				return mid;
+			}else if(n[mid] > target){
+				end = mid - 1;
+			}else{
+				start = mid + 1;
+			}
+		}
+		return -1;
+	}
     public int search(int[] nums, int target) {
-        for(int i =0;i<nums.length;i++){
-            if(nums[i] == target){
-                return i;
-            }
-
+        int peak = pivotOfRotatedArray(nums);
+        if (peak == -1){
+            return index(nums,target,0,nums.length-1);
+        }else if(target < nums[0]){
+            return index(nums,target,peak+1,nums.length -1);
+        }else{
+            return index(nums, target, 0, peak);
         }
-        return -1;
         
     }
+    // public int search(int[] nums, int target) {
+    //     for(int i =0;i<nums.length;i++){
+    //         if(nums[i] == target){
+    //             return i;
+    //         }
+
+    //     }
+    //     return -1;
+        
+    // }
 }
