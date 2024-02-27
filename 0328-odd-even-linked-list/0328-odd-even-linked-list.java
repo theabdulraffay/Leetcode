5e30cd53dbb4eab6f5541bfa1f7a892a6f97c6cd
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2024-02-27 19:03:12 +0500

    Time: 0 ms (100.00%), Space: 44.39 MB (67.45%)

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode even2 = head.next; // taking this ListNode because after the loop when odd is at last we have to
+                                    // connect the two nodes(even and odd), so we store head.next in a temp variable
+        while (odd.next != null && even.next != null) {
+            odd.next = odd.next.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
+        }
+        odd.next = even2;
+        return head;
+
+    }
+}
