092ad8acec98bc6d5d26657331e57d6f0f4c4734
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2024-03-11 17:23:15 +0500

    Time: 1 ms (99.85%), Space: 46.35 MB (18.35%)

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,18 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        for(int i =0;i<nums.length;i++){
-			int counter  = 1;
-			for(int j =0;j<nums.length;j++){
-				if(i == j)continue;
-				if(nums[i] == nums[j]){
-					counter++;
-				}
-			}
-
-			if(counter == 1) return nums[i];
-
-		}
-        return 0;
-        
+        int s = 0;
+        for (int i = 0 ; i < nums.length ; i++) s ^= nums[i];
+        return s;       
     }
 }
