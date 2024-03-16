78abfcdb81f1c93bed5b5ab849bf236a7fbfd31a
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2024-03-16 16:09:34 +0500

    Time: 4 ms (64.47%), Space: 44.75 MB (60.86%)

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,18 +1,51 @@
 class Solution {
+    int search (int[] arr, int numb, boolean last) {
+        int n = arr.length;
+        if(!last) {
+            for(int i = 0; i < n; i++) {
+                if(arr[i] == numb) return i;
+            }
+            return -1;
+            
+        } else {
+            for(int i = n-1; i >= 0; i--) {
+                if(arr[i] == numb) return i;
+            }
+            return -1;
+        }
+    }
     public int[] twoSum(int[] nums, int target) {
-        int[] list = new int[2];
-        for(int i = 0;i<nums.length;i++){
-            for(int j =0;j<nums.length;j++){
-                int temp = nums[i] + nums[j];
-                if(i == j)continue;
-                if(temp == target ){
-                    list[0] = i;
-                    list[1] = j;
-                    return list;
-                }
+        int n = nums.length;
+        int[] t = new int[n];
+        for (int i = 0; i < n; i++) {
+            t[i] = nums[i];
+        }
+        Arrays.sort(t);
+        int st = 0;
+        int en = n - 1;
+        while (st < en) {
+            int temp = t[st] + t[en];
+            if(temp == target) {
+                int first = search (nums, t[st], false);
+                int sec = search (nums, t[en], true);
+                return new int[] {first, sec};
+            }else if (temp > target) {
+                en--;
+            }else {
+                st++;
             }
         }
-        return list;
+        return null;
+
+
+        // for(int i = 0; i < nums.length; i++) {
+        //     for (int j = i + 1; j < nums.length; j++) {
+        //         if(nums[i] + nums[j] == target) {
+        //             return new int[] {i , j};
+        //         }
+        //     }
+        // }
+        // return null;
         
     }
 }
