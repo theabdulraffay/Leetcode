b2782b290ffa950e012386aac79d58d56b67c1dc
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2024-03-17 15:23:31 +0500

    Time: 10 ms (87.36%), Space: 58.31 MB (51.74%)

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i] == nums[i+1]){
-                return true;
-            }
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if(set.contains(i)) {return true;}
+            set.add(i);
         }
         return false;
+
         
     }
 }
