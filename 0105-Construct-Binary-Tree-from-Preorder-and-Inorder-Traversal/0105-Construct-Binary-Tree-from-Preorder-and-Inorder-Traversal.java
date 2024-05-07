e062ae394d7d1f8b45cd970d3084088749ed569a
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2024-05-07 16:17:44 +0500

    Time: 7 ms (13.81%), Space: 84.70 MB (5.76%)

---
+++ 0105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/0105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if(preorder.length == 0) return null; 
+        if(preorder.length == 1) return new TreeNode(preorder[0]);
+        int r = preorder[0];
+        TreeNode root = new TreeNode(r);
+        int index = 0;
+        for(int i = 0; i < preorder.length; i++) {
+            if(inorder[i] == r) {
+                index = i; 
+                break;
+
+            }
+        }
+
+        root.left = buildTree(Arrays.copyOfRange(preorder, 1, index + 1), Arrays.copyOfRange(inorder, 0, index));
+        root.right = buildTree(Arrays.copyOfRange(preorder, index + 1, preorder.length), Arrays.copyOfRange(inorder, index + 1, inorder.length));
+        return root;
+        
+    }
+}
