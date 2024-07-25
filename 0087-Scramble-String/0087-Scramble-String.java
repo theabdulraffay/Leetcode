88035d476334fb3f6cc47ba9e05ff241d05bf28b
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2024-07-25 23:01:14 +0500

    Time: 65 ms (43.47%), Space: 45.75 MB (13.86%)

---
+++ 0087-Scramble-String/0087-Scramble-String.java
@@ -0,0 +1,30 @@
+class Solution {
+    Map<String, Boolean> map = new HashMap<>();
+    public boolean isScramble(String s1, String s2) {
+        int n = s1.length();
+        if (n != s2.length()) {
+            return false;
+        }
+        if(s1.equals(s2) || n == 0) return true;
+
+        String temp = s1 + s2;
+        if(map.containsKey(temp)) return map.get(temp);
+
+        if(n == 1) return false;
+        for(int i = 1; i <= n - 1; i++) {
+            if(isScramble(s1.substring(0, i), s2.substring(0, i)) && 
+            isScramble(s1.substring(i), s2.substring(i))){
+                map.put(temp, true);
+                 return true;
+            }
+
+            if(isScramble(s1.substring(0, i), s2.substring(n - i)) && 
+            isScramble(s1.substring(i), s2.substring(0, n - i))){
+                map.put(temp, true);
+                return true;
+            }
+        }
+        map.put(temp, false);
+        return false;
+    }
+}
