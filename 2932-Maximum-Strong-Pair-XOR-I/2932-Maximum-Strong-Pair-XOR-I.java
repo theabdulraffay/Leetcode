4fb8e8c9594bcfe949e770b63450f63b496a29af
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2024-10-13 21:35:29 +0500

    Time: 2 ms (96.47%), Space: 42.75 MB (88.86%)

---
+++ 2932-Maximum-Strong-Pair-XOR-I/2932-Maximum-Strong-Pair-XOR-I.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumStrongPairXor(int[] nums) {
+        int max = 0;
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i; j < nums.length; j++) {
+                if(Math.abs(nums[i] - nums[j]) <= Math.min(nums[i] , nums[j])) {
+                    max = Math.max(max, nums[i] ^ nums[j]);
+                }
+            }
+        }
+        return max;
+        
+    }
+}
