bf472e45a625e8ce758fa42ce273513d03c1b114
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2025-02-05 01:10:39 +0500

    Time: 0 ms (100.00%), Space: 44.75 MB (60.86%)

---
+++ 1800-Maximum-Ascending-Subarray-Sum/1800-Maximum-Ascending-Subarray-Sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int sum = nums[0], max = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] > nums[i - 1]) sum += nums[i];
+            else {
+                max = Math.max(max, sum);
+                sum = nums[i];
+            }
+        }
+        max = Math.max(max, sum);
+        return max;
+        
+    }
+}
