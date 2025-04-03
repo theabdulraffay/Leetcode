c023a909021776928b1686c6a0a22158b2a64009
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2025-04-03 05:08:49 +0500

    Time: 3 ms (61.47%), Space: 44.75 MB (60.86%)

---
+++ 2874-Maximum-Value-of-an-Ordered-Triplet-II/2874-Maximum-Value-of-an-Ordered-Triplet-II.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        for(int i = 1; i < n; i++) {
+            left[i] = Math.max(left[i - 1], nums[i - 1]);
+            right[n - i - 1] = Math.max(right[n - i], nums[n - i]);
+        }
+
+        long max = 0;
+        for(int i = 0; i < n; i++) {
+            max = Math.max(max, (long)(left[i] - nums[i]) * right[i]);
+        }
+        return max;
+        
+    }
+}
