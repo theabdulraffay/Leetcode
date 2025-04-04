46ec63b7a5a8f969c55a7bb14e97182669473966
Abdul Raffay <81872649+theabdulraffay@users.noreply.github.com>
2025-04-05 01:44:51 +0500

    Time: 0 ms (100.00%), Space: 44.75 MB (60.86%)

---
+++ 0865-Smallest-Subtree-with-all-the-Deepest-Nodes/0865-Smallest-Subtree-with-all-the-Deepest-Nodes.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {TreeNode hehe;
+    int max = 0;
+    int helper(TreeNode root, int depth) {
+        if(root == null) {
+            max = Math.max(max, depth);
+            return depth;
+        }
+        int left = helper(root.left, depth + 1);
+        int right = helper(root.right, depth + 1);
+        if(left == right && left == max) hehe = root;
+        return Math.max(left, right);
+    }
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        helper(root, 0);
+        return hehe;
+        
+    }
+}
